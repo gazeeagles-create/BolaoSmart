c8d5f1dd85b92dd2afd45be3034ddd85650fb77a
gazeeagles-create <gazeeagles@gmail.com>
2025-09-15 13:40:07 -0300

    Create HomePage.tsx

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BolaoCard from '../components/BolaoCard';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao BolãoSmart!</h1>
+      <BolaoCard />
+    </div>
+  );
+};
+
+export default HomePage;
